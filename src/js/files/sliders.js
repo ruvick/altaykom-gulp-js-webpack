acade8923536ba11734dc8b95afedb826712157c
ruvick <rudikov.web@ya.ru>
2023-05-26 12:02:25 +0300

    Вывел слайдер рекомендуем в компонент

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -502,6 +502,63 @@ function initSliders() {
 
 			}
 		});
+		new Swiper('.recommendSl', {
+			// Подключаем модули слайдера
+			// для конкретного случая
+			// modules: [Navigation],
+
+			// effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+
+			observer: true,
+			observeParents: true,
+			slidesPerView: 4,
+			spaceBetween: 20,
+			autoHeight: true,
+			speed: 800,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// Dotts
+			//pagination: {
+			//	el: '.slider-quality__pagging',
+			//	clickable: true,
+			//},
+			// Arrows
+			// navigation: {
+			// 	nextEl: '.slider-info__button-next',
+			// 	prevEl: '.slider-info__button-prev',
+			// },
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			on: {
+
+			}
+		});
 	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
