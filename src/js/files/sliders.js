6288b606c6d341cbd1240fbe16f6c46faa78a6f3
ruvick <rudikov.web@ya.ru>
2023-05-30 11:05:34 +0300

    Подключил второй слайдер товара

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -179,7 +179,7 @@ function initSliders() {
 			speed: 800,
 			//touchRatio: 0,
 			//simulateTouch: false,
-			loop: true,
+			// loop: true,
 			//preloadImages: false,
 			//lazy: true,
 			// Dotts
@@ -388,10 +388,71 @@ function initSliders() {
 
 			}
 		});
+		new Swiper('.cardProductSmallSl', {
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation],
+
+			// effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+
+			observer: true,
+			observeParents: true,
+			slidesPerView: 5,
+			spaceBetween: 12,
+			autoHeight: true,
+			speed: 800,
+			slideToClickedSlide: true,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// Dotts
+			//pagination: {
+			//	el: '.slider-quality__pagging',
+			//	clickable: true,
+			//},
+			// Arrows
+			navigation: {
+				nextEl: '.slider-info__button-next',
+				prevEl: '.slider-info__button-prev',
+			},
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			on: {
+
+			}
+		});
 		new Swiper('.cardProductSl', {
 			// Подключаем модули слайдера
 			// для конкретного случая
 			modules: [Navigation],
+			// thumbs: {
+			// 	swiper: cardProductSmallSl
+			// },
 
 			// effect: 'fade',
 			autoplay: {
@@ -445,6 +506,7 @@ function initSliders() {
 
 			}
 		});
+
 		new Swiper('.boxOrdersL', {
 			// Подключаем модули слайдера
 			// для конкретного случая
