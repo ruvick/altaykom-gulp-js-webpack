107b3d2dcfbdd72256492d4e55196650adad8cd3
ruvick <rudikov.web@ya.ru>
2023-06-10 17:05:59 +0300

    Адаптированна стр Оформление заказа (когда есть профиль)

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -584,7 +584,7 @@ function initSliders() {
 
 			observer: true,
 			observeParents: true,
-			slidesPerView: 3,
+			slidesPerView: 1.2,
 			spaceBetween: 10,
 			autoHeight: true,
 			speed: 800,
@@ -603,27 +603,17 @@ function initSliders() {
 			// 	nextEl: '.slider-info__button-next',
 			// 	prevEl: '.slider-info__button-prev',
 			// },
-			/*
 			breakpoints: {
-				320: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
+				345: {
+					slidesPerView: 1.8,
 				},
 				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
+					slidesPerView: 2.2,
 				},
-				992: {
+				1280: {
 					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
 				},
 			},
-			*/
 			on: {
 
 			}
