d94fcff30451731166f5069f0654483ca55c82a4
ruvick <rudikov.web@ya.ru>
2023-06-12 13:55:25 +0300

    Верстка и адаптив Мультикорзина в футере, мобильная версия

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -173,6 +173,16 @@ if (filterMobSl) {
 	});
 }
 
+// MulticorzineMobFxed
+const multicorzineMobFxed = document.getElementById('multicorzineMobFxed');
+const MobfixedMenuFt = document.getElementById('MobfixedMenuFt');
+
+if (MobfixedMenuFt) {
+	MobfixedMenuFt.addEventListener('click', function () {
+		multicorzineMobFxed.classList.toggle('_active');
+	});
+}
+
 // Range 
 const rangeInput = document.querySelectorAll(".range-input input"),
 	priceInput = document.querySelectorAll(".price-input input"),
